882812e179f39bab9f9c4a545ad1415b75a6dea5
adnanahamedtamim <adnanahamedtamim@gmail.com>
2026-05-24 23:52:08 +0600

    Create k inversions_kattis problem.cpp

---
+++ fft/string matching/k inversions_kattis problem.cpp
@@ -0,0 +1,108 @@
+// problem link : http://open.kattis.com/problems/kinversions
+
+#include <bits/stdc++.h>
+using namespace std;
+
+using cd = complex<double>;
+using ll = long long;
+using vll = vector<ll>;
+const double PI = acos(-1);
+
+void fft(vector<cd> &a, bool invert) {
+    int n = (int)a.size();
+
+    for (int i = 1, j = 0; i < n; i++) {
+        int bit = n >> 1;
+
+        while (j & bit) {
+            j ^= bit;
+            bit >>= 1;
+        }
+
+        j ^= bit;
+
+        if (i < j)
+            swap(a[i], a[j]);
+    }
+
+    for (int len = 2; len <= n; len <<= 1) {
+        double ang = 2 * PI / len * (invert ? -1 : 1);
+
+        cd wlen(cos(ang), sin(ang));
+
+        for (int i = 0; i < n; i += len) {
+            cd w(1);
+
+            for (int j = 0; j < len / 2; j++) {
+                cd u = a[i + j];
+                cd v = a[i + j + len / 2] * w;
+
+                a[i + j] = u + v;
+                a[i + j + len / 2] = u - v;
+
+                w *= wlen;
+            }
+        }
+    }
+
+    if (invert) {
+        for (cd &x : a)
+            x /= n;
+    }
+}
+
+vll multiply(vll &a, vll &b) {
+    vector<cd> fa(a.begin(), a.end());
+    vector<cd> fb(b.begin(), b.end());
+
+    int n = 1;
+
+    while (n < (int)a.size() + (int)b.size())
+        n <<= 1;
+
+    fa.resize(n);
+    fb.resize(n);
+
+    fft(fa, false);
+    fft(fb, false);
+
+    for (int i = 0; i < n; i++)
+        fa[i] *= fb[i];
+
+    fft(fa, true);
+
+    vll res(n);
+
+    for (int i = 0; i < n; i++)
+        res[i] = llround(fa[i].real());
+
+    return res;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    string s;
+    cin >> s;
+    reverse(s.begin(), s.end());
+    ll n = (ll)s.size();
+
+    vll A(n), B(n);
+
+    for (ll i = 0; i < n; i++) {
+        if (s[i] == 'A')
+            A[i] = 1;
+
+        if (s[i] == 'B')
+            B[n - 1 - i] = 1; // reversed
+    }
+
+    vll conv = multiply(A, B);
+
+    for (ll k = 1; k < n; k++) {
+        cout << conv[n - 1 - k] << "\n";
+    }
+
+    return 0;
+}
