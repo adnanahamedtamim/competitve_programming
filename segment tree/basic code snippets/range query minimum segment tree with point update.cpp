bc322ec1446d85571423f76cd274db011e2a9abf
adnanahamedtamim <adnanahamedtamim@gmail.com>
2026-06-15 12:59:23 +0600

    Update range query minimum segment tree with point update.cpp

--- segment tree/basic code snippets/range query minimum segment tree with point update.cpp
+++ segment tree/basic code snippets/range query minimum segment tree with point update.cpp
@@ -1,91 +1,72 @@
-#include <bits/stdc++.h>
-using namespace std;
+class SegmentTree {
+private:
+    ll n;
+    vector<ll> seg;
 
-typedef long long ll;
+    void build(ll node, ll l, ll r, vector<ll>& ara) {
+        if (l == r) {
+            seg[node] = ara[l];
+            return;
+        }
 
+        ll mid = l + (r - l) / 2;
 
-void buildsegtree(ll i,ll l,ll r,vector<ll>& st,vector<ll>& ara){
-  if(l==r){
- st[i]=ara[l];  
- return ;
-  }
-  
-  ll mid=l+(r-l)/2;
-  
-  buildsegtree(2*i+1,l,mid,st,ara);
-  buildsegtree(2*i+2,mid+1,r,st,ara);
-  st[i]=min(st[2*i+1],st[2*i+2]);
-  
-  return;
+        build(2 * node + 1, l, mid, ara);
+        build(2 * node + 2, mid + 1, r, ara);
 
-}
+        seg[node] = min(seg[2 * node + 1], seg[2 * node + 2]);
+    }
 
-ll query(ll start,ll end,ll i,ll l,ll r,vector<ll>& st){
-  if(l>end  || r<start){
-    return INT_MAX;
-  }
-   if(start<=l && end>=r){
-     return st[i];
-   }
-   else{
-      ll mid=l+(r-l)/2;
-      
-      return min(query(start,end,2*i+1,l,mid,st),query(start,end,2*i+2,mid+1,r,st));
-   }
-   
-   return 0;
-}
+    ll query(ll start, ll end, ll node, ll l, ll r) {
+        if (r < start || l > end)
+            return LLONG_MAX;
 
-void  update(ll i,ll l,ll r,ll ind,ll val,vector<ll>& st,vector<ll>& ara){
-  if(l==r){
-    st[i]=val;
-    return;
-  }
-  
-  ll mid=l+(r-l)/2;
-  
-  if(ind<=mid){
-     update(2*i+1,l,mid,ind,val,st,ara);
-  }
-  else{
-    update(2*i+2,mid+1,r,ind,val,st,ara);
-  }
-  st[i]=min(st[2*i+1],st[2*i+2]);
-  
-  return ;
-}
+        if (start <= l && r <= end)
+            return seg[node];
 
+        ll mid = l + (r - l) / 2;
 
-int main() {
-    ll n,q;
- cin >> n >> q;
+        return min(
+            query(start, end, 2 * node + 1, l, mid),
+            query(start, end, 2 * node + 2, mid + 1, r)
+        );
+    }
 
+    void update(ll node, ll l, ll r, ll idx, ll val) {
+        if (l == r) {
+            seg[node] = val;
+            return;
+        }
 
-vector<ll> ara(n);
-for(ll i=0;i<n;i++){
-  cin >> ara[i];
-}
+        ll mid = l + (r - l) / 2;
 
-vector<ll> segtree(4*n,INT_MAX);
-buildsegtree(0,0,n-1,segtree,ara);
+        if (idx <= mid)
+            update(2 * node + 1, l, mid, idx, val);
+        else
+            update(2 * node + 2, mid + 1, r, idx, val);
 
-while(q--){
-  ll a,l,r;
-  cin >> a >> l >> r;
-  
-  
-  if(a==1){
-     l--;
-    
-    update(0,0,n-1,l,r,segtree,ara);
-  }
-  else{
-      cout << query(l-1,r-1,0,0,n-1,segtree) << endl;
-  }
-  
-}
-  
-    return 0;
-}
+        seg[node] = min(seg[2 * node + 1], seg[2 * node + 2]);
+    }
 
+public:
+    // Constructor with initial array
+    SegmentTree(vector<ll>& ara) {
+        n = ara.size();
+        seg.assign(4 * n, LLONG_MAX);
+        build(0, 0, n - 1, ara);
+    }
 
+    // Constructor with size n (all values = 0)
+    SegmentTree(ll size) {
+        n = size;
+        seg.assign(4 * n, 0);
+    }
+
+    ll query(ll l, ll r) {
+        return query(l, r, 0, 0, n - 1);
+    }
+
+    void update(ll idx, ll val) {
+        update(0, 0, n - 1, idx, val);
+    }
+};
