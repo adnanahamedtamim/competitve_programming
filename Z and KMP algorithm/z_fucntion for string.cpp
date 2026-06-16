f115b2816336878e0eefa24996067d229549eea5
adnanahamedtamim <adnanahamedtamim@gmail.com>
2026-06-16 20:39:04 +0600

    Create z_fucntion for string.cpp

---
+++ Z and KMP algorithm/z_fucntion for string.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+vector<ll> z_function(string s) {
+    ll n = s.size();
+    vector<ll> z(n);
+
+    ll l = 0, r = 0;
+
+    for (ll i = 1; i < n; i++) {
+        if (i <= r)
+            z[i] = min(r - i + 1, z[i - l]);
+
+        while (i + z[i] < n && s[z[i]] == s[i + z[i]])
+            z[i]++;
+
+        if (i + z[i] - 1 > r) {
+            l = i;
+            r = i + z[i] - 1;
+        }
+    }
+
+    return z;
+}
