c54b8bf4945297270b1aa6c188b13f06b2df2e89
adnanahamedtamim <adnanahamedtamim@gmail.com>
2026-06-28 23:03:19 +0600

    Update miller rabin test.cpp

--- Primality test/miller rabin test.cpp
+++ Primality test/miller rabin test.cpp
@@ -5,7 +5,7 @@ using ull = unsigned long long;
 using u128 = __uint128_t;
 
 // (a * b) % mod without overflow
-// 7*log2(n)
+// 0(7*log2(n))  almost constant
 
 ull mod_mul(ull a, ull b, ull mod) {
     return (u128)a * b % mod;
