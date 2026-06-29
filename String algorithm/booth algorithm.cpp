f69f9239dad1442ec6430fc6a02c106f70aaa2af
adnanahamedtamim <adnanahamedtamim@gmail.com>
2026-06-29 20:18:11 +0600

    Create booth algorithm.cpp

---
+++ String algorithm/booth algorithm.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+ll booth(string s) {  // booth's algorithm
+// returns the starting index p of the lexicographically smallest substring of a string s 
+    s += s;
+    ll n = s.size() / 2;
+
+    ll i = 0, j = 1, k = 0;
+
+    while (i < n && j < n && k < n) {
+        if (s[i + k] == s[j + k]) {
+            k++;
+        } else if (s[i + k] < s[j + k]) {
+            j += k + 1;
+            if (i == j) j++;
+            k = 0;
+        } else {
+            i += k + 1;
+            if (i == j) i++;
+            k = 0;
+        }
+    }
+
+    return min(i, j);
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    string s;
+    cin >> s;
+
+    ll pos = booth(s);
+
+    cout << (s + s).substr(pos, s.size());
+}
