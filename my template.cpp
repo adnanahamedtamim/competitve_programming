4c42f081e22e47888ced9d34ecbd4bc1e5dbf5ac
adnanahamedtamim <adnanahamedtamim@gmail.com>
2026-09-10 00:48:19 +0600

    Update my template.cpp

--- my template.cpp
+++ my template.cpp
@@ -12,68 +12,55 @@ typedef long double ld;
 
 #define pb push_back
 #define all(x) x.begin(), x.end()
-#define endl '\n'
+// #define endl '\n'
 #define YES cout<<"YES"<<endl
 #define NO cout<<"NO"<<endl
 #define Yes cout<<"Yes"<<endl
 #define No cout<<"No"<<endl
+// #define endl "\n"
  
-#define isSet(x, i) ((x>>i)&1)
-#define setbit(x, i) (x | (1LL<<i))
-#define resetbit(x, i) (x & (~(1LL << i)))
-#define toggleBit(x, i) ((x) ^ (1LL << (i)))
-#define clz(x) __builtin_clzll(x)
-#define ctz(x) __builtin_ctzll(x)
+// #define isSet(x, i) ((x>>i)&1)
+// #define setbit(x, i) (x | (1LL<<i))
+// #define resetbit(x, i) (x & (~(1LL << i)))
+// #define toggleBit(x, i) ((x) ^ (1LL << (i)))
+// #define clz(x) __builtin_clzll(x)
+// #define ctz(x) __builtin_ctzll(x)
 #define csb(x) __builtin_popcountll(x)
-#define msb(x) (ll)((x) ? (63 - __builtin_clzll((ll)(x))) : -1)
-#define lsb(x) (ll)((x) ? (__builtin_ctzll((ll)(x))) : -1)
-#define all(v) v.begin(),v.end()
+// #define msb(x) (ll)((x) ? (63 - __builtin_clzll((ll)(x))) : -1)
+// #define lsb(x) (ll)((x) ? (__builtin_ctzll((ll)(x))) : -1)
 typedef long long ll;
 typedef vector<ll> vll;
 typedef vector<int> vi;
 typedef vector<vector<ll>> vvll;
 typedef vector<vector<int>> vvi;
-typedef pair<ll,ll> pll;
 /*======================
        ordered set for finding kth element in O(logn)
        usage :: find_by_order(pos),,, return the ref to pos'th element(0 based) in the set
                s.order_of_key(x);;; it returns the pos of x(0 based)-> number of elements smaller than x
 =======================*/
-using namespace __gnu_pbds;
+// using namespace __gnu_pbds;
 
-typedef tree<
-ll,         // change the type here
-null_type,
-less<ll>,    // and here
-rb_tree_tag,
-tree_order_statistics_node_update> ordered_set;
-
-/*======================
-   ordered_multiset.. declaration :: ordered_multiset st;
-=======================*/
-
-typedef tree<
-    ll,
-    null_type,
-    less<ll>,
-    rb_tree_tag,
-    tree_order_statistics_node_update
-> ordered_multiset;
+// typedef tree<
+// int,
+// null_type,
+// less<int>,
+// rb_tree_tag,
+// tree_order_statistics_node_update> ordered_set;
 
 /*======================
         CONSTANTS
 =======================*/
 
-const ll MOD = 998244353;
+const ll MOD = 1000000007;
 const ld PI = acos(-1.0L);
 const ld EPS = 1e-12;
 
 const int MAXN = 2e5 + 5;
-const int MAXSIEVE = 2e5 + 5;
+const int MAXSIEVE = 1e6 + 5;
 
 /*======================
         FAST HASH
-=======================*/
+// =======================*/
 
 struct custom_hash {
     static uint64_t splitmix64(uint64_t x) {
@@ -123,6 +110,7 @@ using umap = unordered_map<K,V,custom_hash>;
 =======================*/
 
 ll binexp(ll a,ll b){
+    // if(a==0 || b==0) return 1;
     ll res = 1;
     a %= MOD;
     while(b){
@@ -143,6 +131,7 @@ ll modinv(ll a){
 
 ll fact[MAXN];
 ll invfact[MAXN];
+// vector<ll> primes;
 
 void build_factorial(){
     fact[0] = 1;
@@ -158,29 +147,29 @@ ll ncr(ll n,ll r){
     return fact[n]*invfact[r]%MOD*invfact[n-r]%MOD;
 }
 
-/*======================
-    NUMBER THEORY
-=======================*/
+// /*======================
+//     NUMBER THEORY
+// =======================*/
 
 ll gcd_ll(ll a,ll b){
     if(b==0) return a;
     return gcd_ll(b,a%b);
 }
 
-ll lcm_ll(ll a,ll b){
-    return (a/gcd_ll(a,b))*b;
-}
+// ll lcm_ll(ll a,ll b){
+//     return (a/gcd_ll(a,b))*b;
+// }
 
-ll lcm(ll a, ll b, ll m){
-    if(a==0 || b==0) return m+1;
+// ll lcm(ll a, ll b, ll m){
+//     if(a==0 || b==0) return m+1;
 
-    ll g = gcd_ll(a,b);
-    ll res = a/g;
+//     ll g = gcd_ll(a,b);
+//     ll res = a/g;
 
-    if(res > m/b) return m+1;
+//     if(res > m/b) return m+1;
 
-    return res*b;
-}
+//     return res*b;
+// }
 
 ll lcmwithoutmod(ll a,ll b){
     if(a==0) return b;
@@ -192,56 +181,62 @@ ll lcmwithoutmod(ll a,ll b){
     return res*b;
 }
 
-/*======================
-    SIEVE + FACTORS
-=======================*/
+// /*======================
+//     SIEVE + FACTORS
+// =======================*/
 
-ll spf[MAXSIEVE];
+// vector<ll> primes(MAXSIEVE+2,true);
+// vector<ll> plist;
 
-void build_spf(){
-    for(int i=1;i<MAXSIEVE;i++)
-        spf[i]=i;
-    for(int i=2;i*i<MAXSIEVE;i++){
-        if(spf[i]==i){
-            for(int j=i*i;j<MAXSIEVE;j+=i)
-                if(spf[j]==j)
-                    spf[j]=i;
-        }
-    }
-}
+// void build_seive(){
+//     primes[0]=primes[1]=false;
 
-vector<pair<ll,ll>> prime_factorization(ll x){
-    vector<pair<ll,ll>> factors;
-    while(x>1){
-        ll prime = spf[x];
-        ll cnt = 0;
-        while(x%prime==0){
-            x/=prime;
-            cnt++;
-        }
-        factors.pb({prime,cnt});
-    }
-    return factors;
-}
+//     for(ll i=2;i<=MAXSIEVE;i++){
+//           if(primes[i]==false)continue;
+//           for(ll j=2*i;j<=MAXSIEVE;j+=i){
+//               primes[j]=false;
+//           }
+//     }
+
+//     for(ll i=2;i<=MAXSIEVE;i++){
+//           if(primes[i]){
+//              plist.pb(i);
+//           }
+//     }
+// }
+
+// vector<pair<ll,ll>> prime_factorization(ll x){
+//     vector<pair<ll,ll>> factors;
+//     while(x>1){
+//         ll prime = spf[x];
+//         ll cnt = 0;
+//         while(x%prime==0){
+//             x/=prime;
+//             cnt++;
+//         }
+//         factors.pb({prime,cnt});
+//     }
+//     return factors;
+// }
 
 
-vector<ll> getalldivisors_sorted(ll n){
-   vector<ll> dib;
-   for(ll i=1;i*i<=n;i++){
-       if((n%i==0)){
-            dib.pb(i);
-            if(i*i!=n){
-                dib.pb(n/i);
-            }
-       }
-   }
-  sort(dib.begin(),dib.end());
-  return dib;
-}
+// vector<ll> getalldivisors_sorted(ll n){
+//    vector<ll> dib;
+//    for(ll i=1;i*i<=n;i++){
+//        if((n%i==0)){
+//             dib.pb(i);
+//             if(i*i!=n){
+//                 dib.pb(n/i);
+//             }
+//        }
+//    }
+//   sort(dib.begin(),dib.end());
+//   return dib;
+// }
 
-/*======================
-        INPUT
-=======================*/
+// /*======================
+//         INPUT
+// =======================*/
 
 void get_vector0based(vector<ll>& v){
     for(ll i=0;i<v.size();i++)
@@ -253,108 +248,177 @@ void get_vector1based(vector<ll>& v){
         cin>>v[i];
 }
 
-/*======================
-    BIT TRICK UTILITY
-=======================*/
+// /*======================
+//     BIT TRICK UTILITY
+// =======================*/
 
-long long sum_floor_log2(long long n){
-    long long ans=0;
-    long long power=1;
-    long long k=0;
-    while((power<<1LL)<=n){
-        ans=(ans+k%MOD*power%MOD)%MOD;
-        power<<=1LL;
-        k++;
-    }
-    long long cnt=(n-power+1)%MOD;
-    ans=(ans+k%MOD*cnt%MOD)%MOD;
-    return ans;
-}
+// long long sum_floor_log2(long long n){
+//     long long ans=0;
+//     long long power=1;
+//     long long k=0;
+//     while((power<<1LL)<=n){
+//         ans=(ans+k%MOD*power%MOD)%MOD;
+//         power<<=1LL;
+//         k++;
+//     }
+//     long long cnt=(n-power+1)%MOD;
+//     ans=(ans+k%MOD*cnt%MOD)%MOD;
+//     return ans;
+// }
 
-/*======================
-    3D GEOMETRY
-=======================*/
+// /*======================
+//     3D GEOMETRY
+// =======================*/
 
-struct vec{
-    ld x,y,z;
-};
+// struct vec{
+//     ld x,y,z;
+// };
 
-vec operator+(vec a,vec b){ return {a.x+b.x,a.y+b.y,a.z+b.z}; }
-vec operator-(vec a,vec b){ return {a.x-b.x,a.y-b.y,a.z-b.z}; }
-vec operator/(vec a,ld d){ return {a.x/d,a.y/d,a.z/d}; }
-ld dot(vec a,vec b){ return a.x*b.x + a.y*b.y + a.z*b.z; }
-ld norm(vec a){ return sqrt(dot(a,a)); }
+// vec operator+(vec a,vec b){ return {a.x+b.x,a.y+b.y,a.z+b.z}; }
+// vec operator-(vec a,vec b){ return {a.x-b.x,a.y-b.y,a.z-b.z}; }
+// vec operator/(vec a,ld d){ return {a.x/d,a.y/d,a.z/d}; }
+// ld dot(vec a,vec b){ return a.x*b.x + a.y*b.y + a.z*b.z; }
+// ld norm(vec a){ return sqrt(dot(a,a)); }
 
-/*======================
-    random hash
-=======================*/
-// mersenne twister engine, generates a 64-bit random number
-mt19937_64 rng(
-    chrono::steady_clock::now().time_since_epoch().count()
-);
-
-/*======================
-        Print __int128
-=======================*/
-
-void printInt128(__int128 x) {
-    if (x == 0) {
-        std::cout << 0;
-        return;
-    }
-
-    bool neg = false;
-    if (x < 0) {
-        neg = true;
-        x = -x;
-    }
-
-    std::string s;
-    while (x > 0) {
-        s += (x % 10) + '0';
-        x /= 10;
-    }
-
-    if (neg)
-        s += '-';
-
-    std::reverse(s.begin(), s.end());
-    std::cout << s;
-}
+// /*======================
+//     random hash
+// =======================*/
+// // mersenne twister engine, generates a 64-bit random number
+// mt19937_64 rng(
+//     chrono::steady_clock::now().time_since_epoch().count()
+// );
  
-/*======================
-    string
-=======================*/
-bool small(string a,string b){
-   ll n=a.size();
-   ll m=b.size();
-   ll i=0; 
-   ll j=0;
-   while(i<n && j<m){
-       if(a[i]<b[j]) return true;
-       if(a[i]>b[j]) return false;
-       i++;
-       j++;
-   }
-   if(i==n && j==m) return true;
-   if(j<m) return true;
-   return false;
-}
-
-void solve(ll t){
+// /*======================
+//     string
+// =======================*/
+// bool small(string a,string b){
+//    ll n=a.size();
+//    ll m=b.size();
+//    ll i=0; 
+//    ll j=0;
+//    while(i<n && j<m){
+//        if(a[i]<b[j]) return true;
+//        if(a[i]>b[j]) return false;
+//        i++;
+//        j++;
+//    }
+//    if(i==n && j==m) return true;
+//    if(j<m) return true;
+//    return false;
+// }
 
 
-}
+// struct Fraction {
+//     ll num, den;
+
+//     Fraction(ll n = 0, ll d = 1) {
+//         num = n;
+//         den = d;
+//         simplify();
+//     }
+
+//     void simplify() {
+//         if (den < 0) {
+//             num = -num;
+//             den = -den;
+//         }
+
+//         ll g = gcd(abs(num), abs(den));
+//         num /= g;
+//         den /= g;
+//     }
+
+//     Fraction operator+(const Fraction &other) const {
+//         return Fraction(
+//             num * other.den + other.num * den,
+//             den * other.den
+//         );
+//     }
+
+//     Fraction operator-(const Fraction &other) const {
+//         return Fraction(
+//             num * other.den - other.num * den,
+//             den * other.den
+//         );
+//     }
+
+//     Fraction operator*(const Fraction &other) const {
+//         return Fraction(
+//             num * other.num,
+//             den * other.den
+//         );
+//     }
+
+//     Fraction operator/(const Fraction &other) const {
+//         return Fraction(
+//             num * other.den,
+//             den * other.num
+//         );
+//     }
+// };
+
+
 
 int main()
 {
-  ll t;
-  t=1;
-  cin >> t;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
-  for(ll i=1;i<=t;i++){
-           solve(i);
-  }
+   ll t;
+   cin >> t;
+
+   
+    while(t--){
+
+        ll n,k;
+        cin >> n >> k;
         
+        vll ara(n+1);
+        get_vector1based(ara);
+
+        vll pref(n+1,0);
+
+        for(ll i=1;i<=n;i++){
+             pref[i]=pref[i-1]+ara[i];
+        }
+
+        for(ll i=1;i<=n;i++){
+          
+          if(i==1){
+              cout << 0 << ' ';
+              continue;
+          }
+
+          ll l=i+1,rr=n;
+
+          ll r=i;
+
+          while(l<=rr){
+                ll mid=(l+rr)/2;
+                if((ara[mid]-ara[i-1])>k*(mid-i)){
+                      r=mid;
+                      l=mid+1;
+                }
+                else{
+                      rr=mid-1;
+                }
+          }
+
+          ll sum=pref[r]-pref[i];
+          ll cnt=r-i;
+          ll p=cnt*ara[i-1];
+          
+          ll x=k*(cnt*(cnt+1))/2;
+
+          ll ans=(sum-p-x);
+        //   cout << 'd';
+          cout << ans << ' ';
+        }
+
+   cout << endl;
+
+}
+    
+
     return 0;
 }
